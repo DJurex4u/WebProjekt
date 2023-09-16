ad7b5ff683a739ce81c82718211098cfd2aed2c8
ibudos <ibudos@etfos.hr>
2023-09-16 11:26:38 +0200

    Add project files.

---
+++ NaprednoWebProjekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NaprednoWebProjekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
